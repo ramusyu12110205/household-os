ae95173c0b16957950eb3bd504e9ecb94695bff6
ramusyu12110205 <puyopuyo0831@gmail.com>
2026-08-29 23:19:53 +0900

    Add centralized household state

---
+++ js/core/state.js
@@ -0,0 +1,2 @@
+export const state={user:null,settings:null,summaries:[],payments:[],cards:[],accounts:[],categories:[],budgets:[],transactions:[]};
+export function setState(next){Object.assign(state,next)}
